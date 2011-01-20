694edc6f7be205db0ada5b00058e168aadea905a
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-01-19 22:35:06 -0600

    added logging to background.js

--- js/background.js
+++ js/background.js
@@ -29,6 +29,8 @@ function searchYC(tabId, changeInfo, tab) {
     // the URL used to access searchyc.com's API
     var searchURL = "http://json.searchyc.com/domains/find?url=";
 
+    console.log("Requesting data for url: '" + searchURL + escape(tab.url) + "'");
+
     // issue a synchronous request for the page data
     var req = new XMLHttpRequest();
     req.open("GET", searchURL + escape(tab.url), false);
@@ -36,11 +38,16 @@ function searchYC(tabId, changeInfo, tab) {
 
     // exit if we couldn't access the searchYC server
     if (req.status != 200) {
+        console.error("Could not access searchYC: " + req.status + ", " + req.statusText);
         return;
+    }
+    else {
+        console.log("Got searchYC response");
     }
 
     // parse results
     var results = JSON.parse(req.responseText);
+    console.log("Successfully parsed response JSON, got " + results.length + " results");
 
     // only show the page action icon if there were any results
     if (results.length > 0) {
@@ -56,16 +63,21 @@ function searchYC(tabId, changeInfo, tab) {
         var details = new Object();
         details.tabId = tabId;
         details.title = title;
+
+        console.log("Setting icon title text to: '" + title + "'");
         chrome.pageAction.setTitle(details);
 
         // set the global item id variable
+        console.log("Setting global item id to " + item["id"]);
         ITEM_ID = item["id"];
 
         // show the icon
+        console.log("Showing action icon");
         chrome.pageAction.show(tabId);
     }
     // hide the page action when there are no results
     else {
+        console.log("Hiding action icon");
         chrome.pageAction.hide(tabId);
     }
 }
