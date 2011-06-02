8c189b11df4ebf16ee81c33d8af90e8d8a79da7e
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-06-02 14:45:53 -0500

    changed extension to use the much nicer HNSearch.com api

--- js/background.js
+++ js/background.js
@@ -4,15 +4,16 @@
 // used to allow the popup to access the url of the current page's HN comments
 var ITEM_CACHE = {};
 
-// holds content scraped directly from HN to fill in where search.yc falls down.
-// contains URLs mapped to item id integers.
+// holds content scraped directly from HN to fill in where HNSearch falls down.
+// contains URLs mapped to item id integers. also used to cache items once
+// they've been looked up via the external API.
 var URL_CACHE = {};
 
 // the id of the currently selected tab.  allows the popup to grab the
 // appropriate item id from the global item cache.
 var CURRENT_TAB_ID = -1;
 
-// scrapes content directly from HN to fill in for lag in searchYC database.
+// scrapes content directly from HN to fill in for lag in HNSearch database.
 // updates the global object that holds the URLs in-place.
 var scrapeAndUpdate = function (subdomain, numPages) {
     // bottom out when we've hit our page quota
@@ -118,7 +119,7 @@ var scrapeHandler = function (numNewsPages, numNewestPages) {
     console.log("Scraping " + numNewestPages + " 'newest' pages");
     scrapeAndUpdate("/newest", numNewestPages);
 
-    // count and enumerate URLs in cache
+    // count URLs in cache
     var cacheSize = 0;
     for (var url in URL_CACHE) {
         if (URL_CACHE.hasOwnProperty(url)) {
@@ -129,9 +130,9 @@ var scrapeHandler = function (numNewsPages, numNewestPages) {
     console.log("URL cache has " + cacheSize + " items");
 }
 
-// searches the URL cache, then searchyc.com's archive for the current tab's
+// searches the URL cache, then hnsearch.com's archive for the current tab's
 // URL, then stores its id into a global variable accessible from the popup.
-var searchYC = function (tabId, changeInfo, tab) {
+var search = function (tabId, changeInfo, tab) {
     // don't run when tab isn't loading. keeps from running twice when
     // 'loading' as well as when 'complete'.  we run when 'loading' since it
     // shows the icon almost immediately rather than having to wait for the page
@@ -143,7 +144,7 @@ var searchYC = function (tabId, changeInfo, tab) {
     // used later to set global id and icon visibility
     var newItemId = -1;
 
-    // check the cache for the URL before hitting searchYC
+    // check the cache for the URL before hitting HNSearch
     if (URL_CACHE[tab.url] != null) {
         newItemId = URL_CACHE[tab.url];
         console.log("Found '" + tab.url + "' in cache, item id " + newItemId);
@@ -151,37 +152,38 @@ var searchYC = function (tabId, changeInfo, tab) {
 
     // ask the remote server for the URL
     else {
-        // the URL used to access searchyc.com's API
-        var searchURL = "http://json.searchyc.com/domains/find?url=";
+        // the URL used to access hnsearch.com's API. searches by URL only,
+        // sorts putting newest items first, and limits results to one item.
+        var searchURL = "http://api.thriftdb.com/api.hnsearch.com/items/_search?sortby=create_ts desc&limit=1&filter[fields][url][]=";
 
         console.log("URL not in cache, requesting data for '" + tab.url + "'");
 
         // issue a synchronous request for the page data
         var req = new XMLHttpRequest();
-        req.open("GET", searchURL + escape(tab.url), false);
+        req.open("GET", searchURL + tab.url, false);
         req.send(null);
 
-        // exit if we couldn't access the searchYC server
+        // exit if we couldn't access the HNSearch server
         if (req.status != 200) {
-            console.error("Could not access searchYC: " +
+            console.error("Could not access HNSearch: " +
                     req.status + ", " + req.statusText);
             return;
         }
 
         // parse results
         var results = JSON.parse(req.responseText);
-        console.log("Parsed response JSON, got " + results.length + " results");
+        console.log("Parsed response JSON, got " + results["hits"] + " results");
 
         // only show the page action icon if there were any results
-        if (results.length > 0) {
-            // get the last result, as it's the most recent (and relevant?)
-            item = results[results.length - 1];
+        if (results["hits"] > 0) {
+            // get the result id from the response, which is only one item long
+            item = results["results"][0]["item"];
 
             // set the local item id variable
             newItemId = item["id"];
 
             // add the new URL to the global cache
-            console.log("Adding searchYC response data to global URL cache");
+            console.log("Adding HNSearch response data to global URL cache");
             URL_CACHE[tab.url] = newItemId;
         }
     }
@@ -211,7 +213,7 @@ var updateCurrentTabId = function (tabId, selectInfo) {
 
 // listen for changes to any tab so we can check for HN content for its URL
 console.log("Setting tab update event listener");
-chrome.tabs.onUpdated.addListener(searchYC);
+chrome.tabs.onUpdated.addListener(search);
 
 // listen tor tab selection changes so we can update the current global tab id
 console.log("Setting tab selection change event listener");
@@ -220,8 +222,8 @@ chrome.tabs.onSelectionChanged.addListener(updateCurrentTabId);
 // scrape for content periodically
 var scrapeInterval = 180000; // 3 minutes
 console.log("Setting scrape interval to " + scrapeInterval + " ms");
-setInterval("scrapeHandler(" + 1 + ", " + 2 + ")", scrapeInterval);
+setInterval("scrapeHandler(" + 1 + ", " + 3 + ")", scrapeInterval);
 
 // do an initial scrape, deeper than the periodic one
 console.log("Doing initial content scrape");
-scrapeHandler(5, 10);
+scrapeHandler(2, 5);
