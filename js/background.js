00b089ba6c60fd25ad65c7338dd121aa7cfbb749
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-06-09 03:07:31 -0500

    increased scrape interval to seven minutes, up from three

--- js/background.js
+++ js/background.js
@@ -276,7 +276,7 @@ console.log("Setting tab update event listener");
 chrome.tabs.onUpdated.addListener(searchHandler);
 
 // scrape for content periodically
-var scrapeInterval = 180000; // 3 minutes
+var scrapeInterval = 420000; // 7 minutes
 console.log("Setting scrape interval to " + scrapeInterval + " ms");
 setInterval("scrapeHandler(" + 1 + ", " + 3 + ")", scrapeInterval);
 
